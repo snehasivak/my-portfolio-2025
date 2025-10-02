95e369def1699e8d963f5e692108c3320d922bbb
snehasivak <snehaamal904@gmail.com>
2025-10-02 20:25:16 +0530

    feat: add ClientOnly wrapper to force client rendering

---
+++ app/components/client-only.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+export default function ClientOnly({ children }) {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      {children}
+    </>
+  );
+}
--- app/home-page.jsx
+++ app/home-page.jsx
@@ -1,25 +1,24 @@
 'use client';
 
-import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
+import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
-import dynamic from "next/dynamic";
-
-// Dynamically import the components here, inside a Client Component
-const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
-const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+import Skills from "./components/homepage/skills";
+import ClientOnly from "./components/client-only"; // Import the new component
 
 export default function HomePage({ blogs }) {
   return (
     <div suppressHydrationWarning>
-      <HeroSection />
+      <ClientOnly>
+        <HeroSection />
+        <Skills />
+      </ClientOnly>
+
       <AboutSection />
       <Experience />
-      <Skills />
       <Projects />
       <Education />
       {/* <Blog blogs={blogs} /> */}
