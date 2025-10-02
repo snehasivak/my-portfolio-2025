2268e7c6f8617a786f9d56eaed27240b701ca6b3
snehasivak <snehaamal904@gmail.com>
2025-10-02 20:12:39 +0530

    fix: dynamically import client components

--- app/page.js
+++ app/page.js
@@ -4,21 +4,22 @@ import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+import dynamic from "next/dynamic";
+
+// Dynamically import the components causing the client-side error
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
   const data = await res.json();
-
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
   return filtered;
 };
 
@@ -26,7 +27,7 @@ export default async function Home() {
   const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
